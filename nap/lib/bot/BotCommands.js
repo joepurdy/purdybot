c144e9a461976de8236223bfe6b5a5a6f8ecb985
jurgenzz <jurgis.kacens@outlook.com>
2015-10-02 22:53:41 +0100

    fox says

--- nap/lib/bot/BotCommands.js
+++ nap/lib/bot/BotCommands.js
@@ -142,6 +142,7 @@ var BotCommands = {
                 return "Ring-ding-ding-ding-dingeringeding! Gering-ding-ding-ding-dingeringeding! Gering-ding-ding-ding-dingeringeding! Wa-pa-pa-pa-pa-pa-pow! Wa-pa-pa-pa-pa-pa-pow! Wa-pa-pa-pa-pa-pa-pow! Hatee-hatee-hatee-ho! Hatee-hatee-hatee-ho! Hatee-hatee-hatee-ho! Joff-tchoff-tchoffo-tchoffo-tchoff! Tchoff-tchoff-tchoffo-tchoffo-tchoff! Joff-tchoff-tchoffo-tchoffo-tchoff! Jacha-chacha-chacha-chow! Chacha-chacha-chacha-chow! Chacha-chacha-chacha-chow! Fraka-kaka-kaka-kaka-kow! Fraka-kaka-kaka-kaka-kow! Fraka-kaka-kaka-kaka-kow! A-hee-ahee ha-hee! A-hee-ahee ha-hee! A-hee-ahee ha-hee! A-oo-oo-oo-ooo! Woo-oo-oo-ooo! Wa-wa-way-do Wub-wid-bid-dum-way-do Wa-wa-way-do";
                 
             case 'im batman';
+                
                 return "https://www.youtube.com/watch?v=Y85wj59S94U";
                 
         }
