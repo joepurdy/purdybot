927a9da375698b2f8e6567038c4e2f6e05496ed8
Mark Howard <markhjr@outlook.com>
2015-10-02 14:48:40 -0700

    added my first command :)

--- nap/lib/bot/BotCommands.js
+++ nap/lib/bot/BotCommands.js
@@ -128,6 +128,9 @@ var BotCommands = {
         }
     },
     
+    heygirl: function() {
+        return "http://40.media.tumblr.com/tumblr_lx7125u4h01r8lg7to1_500.png"
+    }
     pbotversion: function(){
         return "botVersion: " + AppConfig.botVersion;
     },
